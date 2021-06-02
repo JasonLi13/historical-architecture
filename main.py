d88f5a312b06a04ab4cf52854c725f9304905cef
Jason <18435@burnside.school.nz>
2021-06-03 08:53:22 +1200

    commit

---
+++ main.py
@@ -0,0 +1,22 @@
+from flask import Flask, render_template
+import sqlite3 as sql
+
+app = Flask(__name__)
+
+
+@app.route('/')
+def home():
+    con = sql.connect("./historical_architecture.db")
+    cur = con.cursor()
+    cur.execute("select * from Ethnicity ORDER BY id;")
+    
+    rows = cur.fetchall(); 
+    return render_template('home.html', title = "Home Tab", row = rows)
+
+@app.route('/country')
+def country():
+    return render_template('country.html', title = "country Tab")
+
+
+if __name__ == "__main__":
+    app.run(debug = True)
