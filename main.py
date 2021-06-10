72a9ae32d3351fb34f0b97ff69eb4db0729f4aaf
Jason <18435@burnside.school.nz>
2021-06-10 12:02:58 +1200

    commit

--- main.py
+++ main.py
@@ -2,6 +2,7 @@ from flask import Flask, render_template
 import sqlite3 as sql
 
 app = Flask(__name__)
+country_items = []
 
 
 @app.route('/')
@@ -9,11 +10,20 @@ def home():
     con = sql.connect("./historical_architecture.db")
     cur = con.cursor()
     cur.execute("select * from Ethnicity ORDER BY id;")
-    
     rows = cur.fetchall(); 
-    cur.execute("select * from Ethnicity join country on ethnicity.id = country.ethnicityid ORDER BY ethnicity.id, country.id;")
-    datas = cur.fetchall
-    return render_template('home.html', title = "Home Tab", row = rows, data = datas)
+
+    cur.execute("select ethnicity.id, country.name, country.photo from Ethnicity join country on ethnicity.id = country.ethnicityid ORDER BY ethnicity.id;")
+    country_items = cur.fetchall(); 
+    ethinicty_country_item = []
+    t = None
+    for i in country_items:
+        if i[0] == t:
+            ethinicty_country_item[-1].append(i)
+        else:
+            t = i[0]
+            ethinicty_country_item.append([i])
+    print(ethinicty_country_item)
+    return render_template('home.html', title = "Home Tab", row = rows, country=ethinicty_country_item)
 
 @app.route('/country')
 def country():
