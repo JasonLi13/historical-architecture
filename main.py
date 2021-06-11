b7db5cfd7b088bd0809818f011ecdabdaab8cbda
Jason <18435@burnside.school.nz>
2021-06-11 19:15:36 +1200

    commit

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, url_for
 import sqlite3 as sql
 
 app = Flask(__name__)
@@ -9,10 +9,10 @@ country_items = []
 def home():
     con = sql.connect("./historical_architecture.db")
     cur = con.cursor()
-    cur.execute("select * from Ethnicity ORDER BY id;")
+    cur.execute("SELECT * FROM Ethnicity ORDER BY id;")
     rows = cur.fetchall(); 
 
-    cur.execute("select ethnicity.id, country.name, country.photo from Ethnicity join country on ethnicity.id = country.ethnicityid ORDER BY ethnicity.id;")
+    cur.execute("SELECT ethnicity.id, country.name, country.photo, country.id FROM Ethnicity join country ON ethnicity.id = country.ethnicityid ORDER BY ethnicity.id;")
     country_items = cur.fetchall(); 
     ethinicty_country_item = []
     t = None
@@ -22,11 +22,13 @@ def home():
         else:
             t = i[0]
             ethinicty_country_item.append([i])
-    print(ethinicty_country_item)
     return render_template('home.html', title = "Home Tab", row = rows, country=ethinicty_country_item)
 
 @app.route('/country')
 def country():
+    con = sql.connect("./historical_architecture.db")
+    cur = con.cursor()
+    cur.execute("SELECT * FROM Country ORDER BY id;")
     return render_template('country.html', title = "country Tab")
 
 
